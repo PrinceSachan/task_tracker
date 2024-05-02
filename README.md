a18090c7a6aefd887b19ab27bb057d0e8f4f4980
PrinceSachan <sachanprince096@gmail.com>
2024-05-03 02:15:53 +0530

    Initialised task_tracker project and worked on some backend part

---
+++ README.md
@@ -0,0 +1,18 @@
+# Task_tracker
+
+Task_tracker is a full-stack web application.
+
+## Backend
+
+Backend is build with
+
+```bash
+Express
+Typescript
+Zod - validation
+Jwt
+Prisma as ORM
+Postgres as database
+```
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ backend/src/zod/user.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+export const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    name: z.string()
+})
+
+export const signinSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6)
+})
